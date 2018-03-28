b153b8f6675bf8a2d189c7d7d4d8a99323701fc7
satish krishnan <satishvckrishnan@gmail.com>
2018-03-28 00:10:00 -0400

    terraform -install-autocomplete  #update Please enter the commit message for your changes. Lines starting

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "test1" {
-  ami           = "ami-d874e0a0"
-  instance_type = "t2.micro"
+  ami                    = "ami-d874e0a0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.Web.id}"]
+
+  user_data = <<-EOF
+              #!bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
     Name = "terraform-test"
+  }
+}
+
+resource "aws_security_group" "Web" {
+  name = "terraform-test-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
